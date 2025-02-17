73257524e6a5b781b95e7cff6df7e19bc4dffc6d
DEV-ZTX <sucidaldazai777@gmail.com>
2025-02-17 20:27:30 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -35,7 +35,7 @@ async def start_pm(client, message: Message, _):
         
         if name.startswith("help"):
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEBAlBnj7y4xNOhQz6boX64khPjYiVj2wACnAsAAsZcIVeYLHxkLmeV9DYE")
+            await message.reply_sticker("CAACAgEAAxkBAAENzwABZ7H96cC6jELihegWHEJZnSxWcQYAAgMJAALjeAQAAaY1zaXqGj3iNgQ")
             return await message.reply_video(
                 video=config.START_VIDEO_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
