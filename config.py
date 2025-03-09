a2f4459e7f6bbdb13391f750fbcafa4eb5a51905
DEV-ZTX <sucidaldazai777@gmail.com>
2025-03-09 09:42:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = "23568641"
 API_HASH = "a39098e8752a45c2d6d1889941547bbc"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "7836466334:AAHo-cjgIGJBJtf5gbucOlkCRCjxi077n2w"
+BOT_TOKEN = "7836466334:AAHwVM9P0UkcTTD2iH6cwxOKZt_28Ae8380"
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = "mongodb+srv://Lord_ichigo:Roshni@cluster0.ytuss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
