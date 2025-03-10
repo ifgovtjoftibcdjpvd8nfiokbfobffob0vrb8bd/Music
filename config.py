dd92f7ed506faf9635e3a19f7f602af5dc6acd0e
DEV-ZTX <sucidaldazai777@gmail.com>
2025-03-10 21:21:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ ADS_MODE = getenv("ADS_MODE", None)
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002323856532"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 8177810307))
+OWNER_ID = int(os.getenv("OWNER_ID", "8177810307").strip().replace("\u200e", ""))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
